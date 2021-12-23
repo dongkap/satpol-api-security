9eb5ef29748e7e3fbf281004226709ef5b7c2205
Ridla Fadilah <ridla.fadilah@gmail.com>
2021-12-24 02:27:38 +0700

    redis stream parameter

--- satpol-security/src/main/java/com/dongkap/security/service/listener/ParameterListenerService.java
+++ satpol-security/src/main/java/com/dongkap/security/service/listener/ParameterListenerService.java
@@ -1,20 +1,27 @@
 package com.dongkap.security.service.listener;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.connection.stream.ObjectRecord;
 import org.springframework.data.redis.connection.stream.RecordId;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.dongkap.common.utils.ParameterStatic;
 import com.dongkap.common.utils.StreamKeyStatic;
 import com.dongkap.dto.common.CommonStreamMessageDto;
 import com.dongkap.dto.master.ParameterI18nDto;
 import com.dongkap.security.common.CommonStreamListener;
+import com.dongkap.security.dao.ParameterI18nRepo;
+import com.dongkap.security.entity.ParameterI18nEntity;
 
 import lombok.SneakyThrows;
 
 @Service
 public class ParameterListenerService extends CommonStreamListener<CommonStreamMessageDto> {
+
+	@Autowired
+	private ParameterI18nRepo parameterI18nRepo;
 
     public ParameterListenerService(
     		@Value("${spring.application.name}") String appName,
@@ -30,11 +37,17 @@ public class ParameterListenerService extends CommonStreamListener<CommonStreamM
         RecordId id = message.getId();
 		LOGGER.info("A message was received stream: [{}], id: [{}]", stream, id);
         CommonStreamMessageDto value = message.getValue();
-        value.getDatas().forEach(data->{
-        	if(data instanceof ParameterI18nDto) {
-        		ParameterI18nDto param = (ParameterI18nDto)data;
-        		LOGGER.info("Parameter Code:[{}], Locale:[{}], Value:[{}]", param.getParameterCode(), param.getLocale(), param.getParameterValue());
-        	}
-        });
+        if(value != null) {
+	        value.getDatas().forEach(data->{
+	        	if(data instanceof ParameterI18nDto) {
+	        		ParameterI18nDto param = (ParameterI18nDto)data;
+	        		ParameterI18nEntity parameterI18n = parameterI18nRepo.findById(param.getParameterI18nUUID()).orElse(null);
+	        		if(parameterI18n != null && value.getStatus() == ParameterStatic.UPDATE_DATA) {
+		        		parameterI18n.setParameterValue(param.getParameterValue());
+		        		parameterI18nRepo.save(parameterI18n);
+	        		}
+	        	}
+	        });
+        }
 	}
 }
