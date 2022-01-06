fac9019fb40219c9ccf2fa0e4d77c2f6e0899c31
Ridla Fadilah <ridla.fadilah@gmail.com>
2022-01-06 22:45:22 +0700

    sorting

--- satpol-security/src/main/java/com/dongkap/security/common/CommonService.java
+++ satpol-security/src/main/java/com/dongkap/security/common/CommonService.java
@@ -10,33 +10,38 @@ import org.springframework.data.domain.Sort.Direction;
 
 import com.google.common.base.CaseFormat;
 
-public class CommonService {;
+public class CommonService {
+	;
 
-    protected Pageable page(Map<String, List<String>> order, int offset, int limit) {
-        int page = 0;
-        if (limit < 10) limit = 10;
-        else if (limit > 250) limit = 250;
-        page = offset / limit;
-        if (page < 0) page = 0;
-        if (order != null && !order.isEmpty()) {
-            Sort sort = null;
-            for (Map.Entry<String, List<String>> direction : order.entrySet()) {
-                if (Direction.ASC.toString().equalsIgnoreCase(direction.getKey())) {
-                    sort = Sort.by(Direction.ASC, (String[]) direction.getValue().toArray());
-                } else {
-                    sort = Sort.by(Direction.DESC, (String[]) direction.getValue().toArray());
-                }
-            }
-            return PageRequest.of(page, limit, sort);
-        }
-        return PageRequest.of(page, limit);
-    }
+	protected Pageable page(Map<String, List<String>> order, int offset, int limit) {
+		int page = 0;
+		if (limit < 10)
+			limit = 10;
+		else if (limit > 250)
+			limit = 250;
+		page = offset / limit;
+		if (page < 0)
+			page = 0;
+		if (order != null && !order.isEmpty()) {
+			Sort sort = null;
+			for (Map.Entry<String, List<String>> direction : order.entrySet()) {
+				String[] propertiesType = new String[direction.getValue().size()];
+				if (Direction.ASC.toString().equalsIgnoreCase(direction.getKey())) {
+					sort = Sort.by(Direction.ASC, direction.getValue().toArray(propertiesType));
+				} else {
+					sort = Sort.by(Direction.DESC, direction.getValue().toArray(propertiesType));
+				}
+			}
+			return PageRequest.of(page, limit, sort);
+		}
+		return PageRequest.of(page, limit);
+	}
 
-    public String convertToCamelCase(String string) {
-        return CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, string);
-    }
+	public String convertToCamelCase(String string) {
+		return CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, string);
+	}
 
-    public String convertToLowerUnderscore(String string) {
-        return CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, string);
-    }
+	public String convertToLowerUnderscore(String string) {
+		return CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, string);
+	}
 }
